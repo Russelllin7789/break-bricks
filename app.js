ed9177bf82fc3dbcf556c8f0051625ea3ffca051
Russell Lin <superbr7789@gmail.com>
2023-03-13 21:00:57 +0800

    add movable floor

--- app.js
+++ app.js
@@ -2,34 +2,46 @@ const canvas = document.getElementById("brick-canvas");
 const canvasWidth = canvas.width;
 const canvasHeight = canvas.height;
 const canvasContext = canvas.getContext("2d");
-let circle_x = 160;
-let circle_y = 60;
+let circleX = 160;
+let circleY = 60;
 let radius = 20;
-let x_speed = 20;
-let y_speed = 20;
+let xSpeed = 20;
+let ySpeed = 20;
+let groundX = 100;
+let groundY = 500;
+let groundHeight = 5;
+
+// add mouse move event within canvas element
+canvas.addEventListener("mousemove", (event) => {
+  groundX = event.clientX;
+});
 
 const drawCircle = () => {
   // check if the ball hit the wall
   // right wall and left wall
-  if (circle_x >= canvasWidth - radius || circle_x <= radius) {
-    x_speed *= -1;
+  if (circleX >= canvasWidth - radius || circleX <= radius) {
+    xSpeed *= -1;
   }
   // bottom wall and top wall
-  if (circle_y >= canvasHeight - radius || circle_y <= radius) {
-    y_speed *= -1;
+  if (circleY >= canvasHeight - radius || circleY <= radius) {
+    ySpeed *= -1;
   }
 
   // change the coordinates of ball
-  circle_x += x_speed;
-  circle_y += y_speed;
+  circleX += xSpeed;
+  circleY += ySpeed;
 
   // reset canvas background
   canvasContext.fillStyle = "black";
   canvasContext.fillRect(0, 0, canvasWidth, canvasHeight);
 
+  // draw movable floor
+  canvasContext.fillStyle = "lightBlue";
+  canvasContext.fillRect(groundX, groundY, 200, groundHeight);
+
   // draw round shaped: x, y, radius, startAngle, endAngle
   canvasContext.beginPath();
-  canvasContext.arc(circle_x, circle_y, radius, 0, 2 * Math.PI);
+  canvasContext.arc(circleX, circleY, radius, 0, 2 * Math.PI);
   canvasContext.stroke();
   canvasContext.fillStyle = "yellow";
   canvasContext.fill();
