f93d66c2c08e4f7096a0ecd4bece75525a575bb9
Russell Lin <superbr7789@gmail.com>
2023-03-13 20:50:38 +0800

    if ball hits wall then bounce back

--- app.js
+++ app.js
@@ -9,6 +9,16 @@ let x_speed = 20;
 let y_speed = 20;
 
 const drawCircle = () => {
+  // check if the ball hit the wall
+  // right wall and left wall
+  if (circle_x >= canvasWidth - radius || circle_x <= radius) {
+    x_speed *= -1;
+  }
+  // bottom wall and top wall
+  if (circle_y >= canvasHeight - radius || circle_y <= radius) {
+    y_speed *= -1;
+  }
+
   // change the coordinates of ball
   circle_x += x_speed;
   circle_y += y_speed;
